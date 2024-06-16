39c30cdadd5ab076fb64943c8dc81601f3eb965d
Kyr001 <cuppastorm@gmail.com>
2024-06-17 00:28:24 +0900

    Add Player Movement&Animation

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,18 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public PlayerCondition condition;
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        condition = GetComponent<PlayerCondition>();
     }
 }
