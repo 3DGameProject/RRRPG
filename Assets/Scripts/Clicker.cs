f54e3510dc2d9c2a513eb45884306cd989f58a77
Kyr001 <cuppastorm@gmail.com>
2024-06-17 00:45:51 +0900

    Add Cliker

---
+++ Assets/Scripts/Clicker.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class ClickerGameNewInput : MonoBehaviour
+{
+    public Text scoreText;
+
+    private int score = 0;
+    private int clickPower = 10;
+
+    private InputAction clickAction;
+
+    void OnEnable()
+    {
+        clickAction = new InputAction(binding: "<Mouse>/leftButton");
+        clickAction.Enable();
+
+        clickAction.started += ctx => ClickButtonClicked();
+    }
+
+    void OnDisable()
+    {
+        clickAction.Disable();
+    }
+
+    void ClickButtonClicked()
+    {
+        score += clickPower;
+        UpdateUI();
+    }
+
+    void UpdateUI()
+    {
+        scoreText.text = score.ToString(); 
+    }
+}
